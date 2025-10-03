386e9b7ace6feee9893d38c284d1cfb44a395a42
prathmeshns <prathmeshns23@gmail.com>
2025-10-04 00:09:12 +0530

    change vite.config file for target

--- vite.config.js
+++ vite.config.js
@@ -18,7 +18,7 @@ export default defineConfig({
     ],
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'https://backend-idcardbitroot.onrender.com',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
